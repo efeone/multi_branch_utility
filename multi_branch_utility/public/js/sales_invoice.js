ebf3a368d6ca84078e458aff213590d7a97fe8d9
muhammadmp <mammuz77@gmail.com>
2022-08-30 10:13:00 +0530

    fix: sales invoice minor  fixing

--- multi_branch_utility/public/js/sales_invoice.js
+++ multi_branch_utility/public/js/sales_invoice.js
@@ -1,6 +1,9 @@
 frappe.ui.form.on('Sales Invoice', {
     onload_post_render(frm) {
         frm.remove_custom_button('Fetch Timesheet')
+    },
+    refresh(frm){
+        frm.remove_custom_button('Fetch Timesheet')
     }
 });
 
