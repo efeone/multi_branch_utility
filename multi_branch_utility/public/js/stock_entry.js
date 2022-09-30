2ba527bf3277bfe9ae3402876088b5c93cefd34b
sherinkr <sherinkrply@gmail.com>
2022-09-30 16:05:01 +0530

    fix: Ignore user permission for Material Transfer

--- multi_branch_utility/public/js/stock_entry.js
+++ multi_branch_utility/public/js/stock_entry.js
@@ -1,5 +1,6 @@
 frappe.ui.form.on('Stock Entry', {
 	refresh(frm) {
+		ignore_permissions_for_transfer(frm);
 		frm.set_query("stock_entry_type", function() {
 			return {
 				filters: {
@@ -7,5 +8,19 @@ frappe.ui.form.on('Stock Entry', {
 				}
 			};
 		});
+	},
+	stock_entry_type(frm) {
+		ignore_permissions_for_transfer(frm);
 	}
 });
+
+function ignore_permissions_for_transfer(frm){
+	if(frm.doc.stock_entry_type && frm.doc.stock_entry_type == 'Material Transfer' ){
+		frm.set_df_property('to_warehouse', 'ignore_user_permissions', 1);
+		frm.refresh_field('to_warehouse');
+	}
+	else{
+		frm.set_df_property('to_warehouse', 'ignore_user_permissions', 0);
+		frm.refresh_field('to_warehouse');
+	}
+}
