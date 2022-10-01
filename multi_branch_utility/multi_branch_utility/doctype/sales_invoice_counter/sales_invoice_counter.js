5de3851fa254d44f1a22fa97dc42a865f5ebe366
Aadith <aadithp210@gmail.com>
2022-10-01 13:42:46 +0530

    feat:create a validation in sales invoice counter doctype to create invoice by multiple user

--- multi_branch_utility/multi_branch_utility/doctype/sales_invoice_counter/sales_invoice_counter.js
+++ multi_branch_utility/multi_branch_utility/doctype/sales_invoice_counter/sales_invoice_counter.js
@@ -1,8 +1,62 @@
 // Copyright (c) 2022, efeone Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
+{% include 'erpnext/selling/sales_common.js' %};
+frappe.provide("erpnext.accounts");
+
+erpnext.accounts.SalesInvoiceController = erpnext.selling.SellingController.extend({
+
+});
+$.extend(cur_frm.cscript, new erpnext.accounts.SalesInvoiceController({frm: cur_frm}));
 frappe.ui.form.on('Sales Invoice Counter', {
 	refresh: function(frm) {
-		$('.form-sidebar').hide();
+		frm.disable_save();
+		frm.refresh_fields()
+		create_sales_invoice_btn(frm)
 	}
 });
+function create_sales_invoice_btn(frm){
+	frm.add_custom_button(__('Submit Invoice'), function(){
+		create_invoice(frm)
+	}).addClass("btn-primary");
+}
+function create_invoice(frm){
+	if(cur_frm.doc.items.length){
+		frappe.db.insert({
+			doctype: 'Sales Invoice',
+			customer: frm.doc.customer,
+			payment_type:frm.doc.payment_type,
+			items: frm.doc.items,
+			docstatus:1
+		}).then(function(doc) {
+			make_payment(frm,doc)
+		});
+	}else{
+		frappe.msgprint({
+			title: __('Notification'),
+			indicator: 'red',
+			message: __('Select Item')
+		});
+	}
+
+}
+function make_payment(frm,doc){
+	frappe.call({
+		method:'multi_branch_utility.multi_branch_utility.doctype.sales_invoice_counter.sales_invoice_counter.make_payment',
+		args:{
+			'self':doc.name
+		},
+		callback:function(r){
+			if(r){
+				frm.reload_doc()
+				frappe.show_alert({
+					message:__('Invoiced'),
+					indicator:'red'
+				}, 5);
+				window.open("/printview?doctype=Sales%20Invoice&name="+ doc.name +"&trigger_print=1&format=KAS%20VAT%20Invoice%20New&no_letterhead=0&letterhead=Gateway%20Wholesale&settings=%7B%7D&_lang=en-US")
+			}
+		},
+		freeze: true,
+	 freeze_message: ('Creating Sales Invoice.!!')
+	})
+}
--- multi_branch_utility/multi_branch_utility/doctype/sales_invoice_counter/sales_invoice_counter.py
+++ multi_branch_utility/multi_branch_utility/doctype/sales_invoice_counter/sales_invoice_counter.py
@@ -1,8 +1,38 @@
 # Copyright (c) 2022, efeone Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.model.mapper import get_mapped_doc
+from erpnext.accounts.doctype.sales_invoice.sales_invoice import get_bank_cash_account
 
 class SalesInvoiceCounter(Document):
 	pass
+
+@frappe.whitelist()
+def make_payment(self):
+	doc=frappe.get_doc("Sales Invoice",self)
+	if frappe.db.get_single_value('Multi Branch Settings','allow_payment_entry'):
+		if not doc.is_return and doc.payment_type and doc.payment_type=='CASH':
+			mode_of_payment = frappe.get_doc("Mode of Payment", 'Cash')
+			mode_of_payment_account = mode_of_payment.accounts[0].default_account
+			company = frappe.get_last_doc('Company')
+			pay = frappe.new_doc('Payment Entry')
+			pay.payment_type = 'Receive'
+			pay.mode_of_payment = "Cash"
+			pay.party_type = 'Customer'
+			pay.party = doc.customer
+			pay.paid_from = company.default_receivable_account
+			pay.source_exchange_rate = 1
+			pay.paid_amount = doc.outstanding_amount
+			pay.received_amount = doc.outstanding_amount
+			pay.paid_to = mode_of_payment_account
+			pay.append("references",{
+			"reference_doctype" : doc.doctype,
+			"reference_name": doc.name,
+			"total_amount": doc.grand_total,
+			"outstanding_amount": doc.outstanding_amount,
+			"allocated_amount": doc.outstanding_amount
+			})
+			pay.submit()
+			return pay
