32e05a4ddfa311ff61936d2a43ae9807e7654ddd
SherinKR <sherinkrply@gmail.com>
2022-10-06 12:04:29 +0530

    feat: Added Is Default option in Payment Type Table

--- multi_branch_utility/multi_branch_utility/utils.py
+++ multi_branch_utility/multi_branch_utility/utils.py
@@ -8,8 +8,9 @@ def get_mode_of_payment(payment_type, cost_center):
     payment_type_doc = frappe.get_doc('Payment Type', payment_type )
     if payment_type_doc.payment_type_accounts:
         for payment_type_account in payment_type_doc.payment_type_accounts:
-            if payment_type_account.cost_center == cost_center:
-                payment_type_details = payment_type_account
+            if not payment_type_details or payment_type_account.is_default == 1:
+                if payment_type_account.cost_center == cost_center:
+                    payment_type_details = payment_type_account
     return payment_type_details
 
 @frappe.whitelist()
