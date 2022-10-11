1dd110139845b99931b69ecbd5c29ce84aa249f8
rijilprakashan <rijilprakashan@gmail.com>
2022-10-11 17:12:31 +0530

    feat: Fetching value from customer & supplier

--- multi_branch_utility/public/js/sales_invoice.js
+++ multi_branch_utility/public/js/sales_invoice.js
@@ -7,6 +7,7 @@ frappe.ui.form.on('Sales Invoice', {
       window.open("/printview?doctype=Sales%20Invoice&name="+ frm.doc.name +"&trigger_print=1&format=KAS%20VAT%20Invoice%20New&no_letterhead=0&letterhead=Gateway%20Wholesale&settings=%7B%7D&_lang=en-US")
     },
     refresh(frm){
+        frm.set_value('update_stock', 1);
         frm.remove_custom_button('Fetch Timesheet')
         frm.set_query("customer", function() {
     			return {
@@ -16,6 +17,25 @@ frappe.ui.form.on('Sales Invoice', {
     			};
     		});
     },
+    customer: function(frm){
+        if(frm.doc.customer){
+            frappe.call({
+                'method': 'frappe.client.get',
+                args: {
+                    doctype: 'Customer',
+                    name: frm.doc.customer
+                },
+                callback: function (data) {
+                    if(data.message){
+                        frm.set_value('payment_type', data.message.payment_type)
+                        frm.set_value('set_warehouse', data.message.default_warehouse)
+                        frm.set_value('selling_price_list', data.message.default_price_list)
+                        frm.refresh_fields()
+                    }
+                }
+            })
+        }
+    },    
 });
 frappe.ui.form.on('Sales Invoice Item', {
     item_code: function (frm, cdt, cdn) {
